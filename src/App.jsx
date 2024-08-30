b14170178d460a577fb2e94403ecaf1375b77540
Zahra Rostami <zahrarostami137878@gmail.com>
2024-08-30 16:09:14 +0330

    add Header and Footer props

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <Header/>
-        <Footer />
+        <Header blogTitle = "Zahra Rostami's Blog"/>
+        <Footer title="You're the reason we get out of bed in the morning... well, after snoozing a few times."/>
     </div>
   );
 }
