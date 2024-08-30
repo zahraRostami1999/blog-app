0fb17eb6f82defafc389b04ae128bd93b010a85d
Zahra Rostami <zahrarostami137878@gmail.com>
2024-08-30 13:22:15 +0330

    edit App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header>
         <Header/>
-      </header>
-      <div>
         <Footer />
-      </div>
     </div>
   );
 }
