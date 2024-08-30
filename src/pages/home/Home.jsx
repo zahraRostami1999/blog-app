8bab1def1f97bd2a067f9e98e2859de66c92648e
Zahra Rostami <zahrarostami137878@gmail.com>
2024-08-30 16:40:29 +0330

    add Header and Footer in home page

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import Header from "../../Components/header/Header";
+import Footer from "../../Components/footer/Footer";
+
+function Home() {
+  return (
+    <div>
+      <Header blogTitle="Zahra Rostami's Blog" />
+      <Footer title="You're the reason i get out of bed in the morning... well, after snoozing a few times." />
+    </div>
+  );
+}
+
+export default Home;
