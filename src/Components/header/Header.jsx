5d4990930cb54bf610b06b13cdece216899c6bcd
Zahra Rostami <zahrarostami137878@gmail.com>
2024-08-30 13:19:54 +0330

    edit Header component sections

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -1,11 +1,20 @@
 import styled from "./header.module.css";
+import logo from './Blog-app-logo.png';
 
 function Header() {
   return (
     <header className={styled.header}>
-        <div className={styled.logo}>Logo</div>
-        <div className={styled.navbar}>Navbar</div>
-        <div className={styled.switch}>Switch</div>
+        <div className={styled.container}>
+            <div className={styled.logo}>
+                <div className={styled.logoImg}>
+                    <img src={logo} alt="logo" />
+                </div>
+                <div className={styled.logoTitle}><h3>Zahra Rostami</h3></div>
+            </div>
+            <div className={styled.navbar}>Navbar</div>
+            <div className={styled.switch}>Switch</div>
+        </div>
+
     </header>
   );
 }
