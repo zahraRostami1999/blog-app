3cc4d8f8b15c0600173df3deebab2d175cea0920
Zahra Rostami <zahrarostami137878@gmail.com>
2024-08-31 19:49:14 +0330

    add img and title div for Post

--- src/Components/post/Post.jsx
+++ src/Components/post/Post.jsx
@@ -1,9 +1,11 @@
 import styled from './post.module.css'
+import cover1 from './../../assets/image/post1.png';
 
 function Post(){
     return(
-        <div className={styled.post}>
-            <div>Post</div>
+        <div className={styled.postWrapper}>
+            <div className={styled.postImg}></div>
+            <div className={styled.postTitle}>Post</div>
         </div>
     );
 }
