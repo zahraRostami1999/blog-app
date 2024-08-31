7ee2b352716efa5ef01473bf20e5f9539da510cf
Zahra Rostami <zahrarostami137878@gmail.com>
2024-08-31 11:57:48 +0330

    add Post component to home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import Header from "../../Components/header/Header";
 import Footer from "../../Components/footer/Footer";
+import Post from "../../Components/post/Post";
 
 function Home() {
   return (
     <div>
       <Header blogTitle="Zahra Rostami's Blog" />
       <Footer title="You're the reason i get out of bed in the morning... well, after snoozing a few times." />
+      <Post />
     </div>
   );
 }
