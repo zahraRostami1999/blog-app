4abb7ca1f1b087d5170040d0a838ef36459c948b
Zahra Rostami <zahrarostami137878@gmail.com>
2024-08-31 13:20:43 +0330

    edit Post component

---
+++ src/Components/post/Post.jsx
@@ -0,0 +1,11 @@
+import styled from './post.module.css'
+
+function Post(){
+    return(
+        <div className={styled.post}>
+            <div>Post</div>
+        </div>
+    );
+}
+
+export default Post;
