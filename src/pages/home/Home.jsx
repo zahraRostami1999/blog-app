728b1e68123886ed3038bb893b5b6745943937d5
Zahra Rostami <zahrarostami137878@gmail.com>
2024-08-31 12:11:26 +0330

    edit blog title name to XanimQiz(JEYRAN)'s Blog

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,7 +5,7 @@ import Post from "../../Components/post/Post";
 function Home() {
   return (
     <div>
-      <Header blogTitle="Zahra Rostami's Blog" />
+      <Header blogTitle="XanimQiz(JEYRAN)'s Blog" />
       <Footer title="You're the reason i get out of bed in the morning... well, after snoozing a few times." />
       <Post />
     </div>
