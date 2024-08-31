9507067895b31aea4b5981606822113fd60de1c7
Zahra Rostami <zahrarostami137878@gmail.com>
2024-08-31 13:17:54 +0330

    app Post component to home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,13 @@
 import Header from "../../Components/header/Header";
 import Footer from "../../Components/footer/Footer";
-import Post from "../../Components/post/Post";
+import Post_list from "../../Components/post_list/Post_list";
 
 function Home() {
   return (
     <div>
       <Header blogTitle="XanimQiz(JEYRAN)'s Blog" />
+      <Post_list />
       <Footer title="You're the reason i get out of bed in the morning... well, after snoozing a few times." />
-      <Post />
     </div>
   );
 }
