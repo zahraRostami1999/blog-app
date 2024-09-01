b6c2d4d45ba74c91c4306f78e9841054b65cee8d
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-01 20:16:58 +0330

    add contents to Post with props: pass post's badge

--- src/Components/post/Post.jsx
+++ src/Components/post/Post.jsx
@@ -4,10 +4,10 @@ import cover1 from "./../../assets/image/post1.png";
 function Post(props) {
   return (
     <div className={styled.postWrapper}>
-      <div className={styled.postId}>{props.content.id}</div>
       <div className={styled.postImg}>{props.content.cover}
         <img src={cover1} alt="cover" />
       </div>
+      <div className={styled.postBadge}>{props.content.badge}</div>
       <div className={styled.postTitle}>{props.content.title}</div>
       <div className={styled.postAuthor}>{props.content.author}</div>
       <div className={styled.postTime}>{props.content.readingTime}</div>
