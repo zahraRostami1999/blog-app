08f46e0f40aa3955be6130b1e5ff9a3589cc9586
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-01 20:28:00 +0330

    add author and reading time to same div

--- src/Components/post/Post.jsx
+++ src/Components/post/Post.jsx
@@ -4,13 +4,16 @@ import cover1 from "./../../assets/image/post1.png";
 function Post(props) {
   return (
     <div className={styled.postWrapper}>
-      <div className={styled.postImg}>{props.content.cover}
+      <div className={styled.postImg}>
+        {props.content.cover}
         <img src={cover1} alt="cover" />
       </div>
       <div className={styled.postBadge}>{props.content.badge}</div>
       <div className={styled.postTitle}>{props.content.title}</div>
-      <div className={styled.postAuthor}>{props.content.author}</div>
-      <div className={styled.postTime}>{props.content.readingTime}</div>
+      <div className={styled.postFooter}>
+        <div className={styled.postAuthor}>{props.content.author}</div>
+        <div className={styled.postTime}>{props.content.readingTime}</div>
+      </div>
     </div>
   );
 }
