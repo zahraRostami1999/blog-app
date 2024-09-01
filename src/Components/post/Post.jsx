5bbfa36445a8495d71dfcaefc5a7e671b2f4d178
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-01 18:42:07 +0330

    add content for Post sections

--- src/Components/post/Post.jsx
+++ src/Components/post/Post.jsx
@@ -4,14 +4,14 @@ import cover1 from "./../../assets/image/post1.png";
 function Post() {
   return (
     <div className={styled.postWrapper}>
-      <div className={styled.postId}></div>
+      <div className={styled.postId}>1</div>
       <div className={styled.postImg}>
         <img src={cover1} alt="cover" />
       </div>
-      <div className={styled.postTitle}>Post</div>
-      <div className={styled.postAuthor}></div>
-      <div className={styled.postContent}></div>
-      <div className={styled.postTime}></div>
+      <div className={styled.postTitle}>First Post</div>
+      <div className={styled.postAuthor}>Bob Johnson</div>
+      <div className={styled.postContent}>This is the first post in our API</div>
+      <div className={styled.postTime}>5 minutes</div>
     </div>
   );
 }
