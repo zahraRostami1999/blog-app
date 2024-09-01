ffe3132f74cee05421b807d029d6880b77a59d8a
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-01 18:23:49 +0330

    add post id div and class to Post

--- src/Components/post/Post.jsx
+++ src/Components/post/Post.jsx
@@ -1,15 +1,19 @@
-import styled from './post.module.css'
-import cover1 from './../../assets/image/post1.png';
+import styled from "./post.module.css";
+import cover1 from "./../../assets/image/post1.png";
 
-function Post(){
-    return(
-        <div className={styled.postWrapper}>
-            <div className={styled.postImg}>
-                <img src={cover1} alt="cover" />
-            </div>
-            <div className={styled.postTitle}>Post</div>
-        </div>
-    );
+function Post() {
+  return (
+    <div className={styled.postWrapper}>
+      <div className={styled.postId}></div>
+      <div className={styled.postImg}>
+        <img src={cover1} alt="cover" />
+      </div>
+      <div className={styled.postTitle}>Post</div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
 }
 
 export default Post;
