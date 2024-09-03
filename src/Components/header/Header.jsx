61227bec1a8cbb75e7ea33f98f8feaf5db2256bc
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-03 22:17:07 +0330

    change navbar style class to navbarContainer

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -1,21 +1,24 @@
 import styled from "./header.module.css";
-import logo from './../../assets/image/Blog-app-logo.png';
+import logo from "./../../assets/image/Blog-app-logo.png";
 
 function Header(props) {
   return (
     <header className={styled.header}>
-        <div className={styled.container}>
-            <div className={styled.logo}>
-                <div className={styled.logoImg}>
-                    <img src={logo} alt="logo" />
-                </div>
-                <div className={styled.logoTitle}><h3>{props.blogTitle}</h3></div>
-            </div>
-            <div className={styled.navbar}>Navbar</div>
-            <div className={styled.switch}> switch
-            </div>
+      <div className={styled.container}>
+        <div className={styled.logo}>
+          <div className={styled.logoImg}>
+            <img src={logo} alt="logo" />
+          </div>
+          <div className={styled.logoTitle}>
+            <h3>{props.blogTitle}</h3>
+          </div>
         </div>
+        <div className={styled.navbarContainer}>
 
+          <a href="#home">Home</a>
+        </div>
+        <div className={styled.switch}> switch</div>
+      </div>
     </header>
   );
 }
