9e738f87241a41ecb89ac70a7191363426ed7c83
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-03 21:33:47 +0330

    add AboutUs page with route in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import Home from './pages/home/Home'
-import './App.css';
-import {Route, Routes} from 'react-router-dom';
+import Home from "./pages/home/Home";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
       </Routes>
     </div>
   );
