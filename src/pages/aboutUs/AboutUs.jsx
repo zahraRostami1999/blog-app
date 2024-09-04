65d6e85447c8116796e342d182867a9a5a47f208
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-04 19:59:28 +0330

    add blogTitle to Header's that put in AboutUs component

--- src/pages/aboutUs/AboutUs.jsx
+++ src/pages/aboutUs/AboutUs.jsx
@@ -3,7 +3,7 @@ import Header from "../../Components/header/Header";
 function AboutUs(){
     return(
         <div>
-            <Header />
+            <Header blogTitle="XanimQiz(Jeyran Qulu)'s Blog" />
             <div>This is About Us Page</div>
         </div>
     );
