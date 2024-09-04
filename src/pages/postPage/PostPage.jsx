6c3e1f0cf822d66192246837655c8c57d385c5fa
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-04 20:39:38 +0330

    add Header component to PostPage

--- src/pages/postPage/PostPage.jsx
+++ src/pages/postPage/PostPage.jsx
@@ -1,8 +1,10 @@
-import styled from './postPage.module.css'
+import styled from './postPage.module.css';
+
 
 function PostPage(){
     return(
         <div>
+            <Header blogTitle="XanimQiz(Jeyran Qulu)'s Blog" />
             <h1>Post Page</h1>
         </div>
     );
