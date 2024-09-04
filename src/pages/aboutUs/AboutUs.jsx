741f978850fb24b4fa63d9ced01095cc00fb8bef
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-04 19:58:06 +0330

    add Header to AboutUs component

--- src/pages/aboutUs/AboutUs.jsx
+++ src/pages/aboutUs/AboutUs.jsx
@@ -3,6 +3,7 @@ import Header from "../../Components/header/Header";
 function AboutUs(){
     return(
         <div>
+            <Header />
             <div>This is About Us Page</div>
         </div>
     );
