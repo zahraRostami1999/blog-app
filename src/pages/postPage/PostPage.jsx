37132c5593b12d5b77ebc39594cfd152a034fdef
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-04 20:46:14 +0330

    add postPage class to PostPage component

--- src/pages/postPage/PostPage.jsx
+++ src/pages/postPage/PostPage.jsx
@@ -3,7 +3,7 @@ import Header from '../../Components/header/Header';
 
 function PostPage(){
     return(
-        <div>
+        <div className={styled.postPage}>
             <Header blogTitle="XanimQiz(Jeyran Qulu)'s Blog" />
             <h1>Post Page</h1>
         </div>
