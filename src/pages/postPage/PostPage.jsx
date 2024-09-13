5ab32f226748021bc416326b50c95e26143c9ba6
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-13 19:47:21 +0330

    add post's title and authot and readingTime to div that class name is postTitle

--- src/pages/postPage/PostPage.jsx
+++ src/pages/postPage/PostPage.jsx
@@ -19,9 +19,12 @@ function PostPage() {
       </div>
       <div className={styled.postContainer}>
         <div className={styled.postSection}>
-          <div>
-            <h1>{post.title}</h1>
+          <div className={styled.postTitle}>
+            <h2>{post.title}</h2>
+            <h5>{post.author}</h5>
+            <h5>{post.readingTime}</h5>
           </div>
+          
         </div>
       </div>
     </div>
