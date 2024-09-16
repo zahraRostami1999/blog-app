90e05909c5778c2978eefdbdfd48b72c176ae39d
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-16 21:03:52 +0330

    edit Home component: convert first div to fragment

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,15 @@
 import Header from "../../Components/header/Header";
 import Footer from "../../Components/footer/Footer";
 import Post_list from "../../Components/post_list/Post_list";
+import { Fragment } from "react";
 
 function Home() {
   return (
-    <div>
+    <>
       <Header blogTitle="XanimQiz(Jeyran Qulu)'s Blog" />
       <Post_list />
       <Footer title="You're the reason I get out of bed in the morning... well, after snoozing a few times." />
-    </div>
+    </>
   );
 }
 
