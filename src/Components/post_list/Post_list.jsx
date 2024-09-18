2c30956c3b798945486c48ec23070e4657942f6a
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-18 19:15:27 +0330

    edit Post_list component: define isLoadin and set state

--- src/Components/post_list/Post_list.jsx
+++ src/Components/post_list/Post_list.jsx
@@ -6,32 +6,40 @@ import axios from "axios";
 
 function Post_list() {
   const [postContent, setpostContent] = useState([]);
+  const [isLoading, setisLoading] = useState(false);
 
   useEffect(() => {
+    setisLoading(true);
     axios
       .get("http://localhost:8000/PostAPI")
       .then((result) => {
         setpostContent(result.data);
+        setisLoading(false);
       })
       .catch((error) => {
         console.log("Error fetching data", error);
+        setisLoading(false);
       });
   }, []);
 
   return (
     <div className="container">
-      <div className={styled.postList}>
-        <div>
-          <h2>Posts list</h2>
+      {isLoading ? (
+        "Loading..."
+      ) : (
+        <div className={styled.postList}>
+          <div>
+            <h2>Posts list</h2>
+          </div>
+          <div className={styled.list}>
+            {postContent.map((result) => (
+              <Link to={`/Post/${result.id}`}>
+                <Post key={result.id} content={result} />
+              </Link>
+            ))}
+          </div>
         </div>
-        <div className={styled.list}>
-          {postContent.map((result) => (
-            <Link to={`/Post/${result.id}`}>
-              <Post key={result.id} content={result} />
-            </Link>
-          ))}
-        </div>
-      </div>
+      )}
     </div>
   );
 }
