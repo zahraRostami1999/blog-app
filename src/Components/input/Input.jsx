8a2b7146091acad57ab2f74fcd98c044c901c652
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-18 21:16:37 +0330

    create Input component

---
+++ src/Components/input/Input.jsx
@@ -0,0 +1,15 @@
+import styled from "./input.module.css";
+import { Fragment } from "react";
+
+function Input() {
+  return (
+    <>
+      <div>
+        <label htmlFor=""></label>
+        <input type="text" />
+      </div>
+    </>
+  );
+}
+
+export default Input;
