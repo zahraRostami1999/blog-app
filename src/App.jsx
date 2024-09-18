5123874a4397e0e278c0c729c80bf6144a0207c9
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-18 12:53:37 +0330

    import PostsList to App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import AboutUs from "./pages/aboutUs/AboutUs";
 import PostPage from "./pages/postPage/PostPage";
+import PostsList from "./pages/postsList/PostsList";
 import "./App.css";
 
 
