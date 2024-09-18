e8eb9cedfd5d069e25075ccb82afe6f6a79cf1f1
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-18 12:56:07 +0330

    import styles to Home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,16 @@
 import Header from "../../Components/header/Header";
 import Footer from "../../Components/footer/Footer";
 import { Fragment } from "react";
+import styles from "./home.module.css";
 
 function Home() {
   return (
     <>
-      <Header blogTitle="Zahra Rostami(XanimQiz)'s Blog" />
-      <h1>Hello My friend</h1>
-      <Footer title="..." />
+      <div>
+        <Header blogTitle="Zahra Rostami(XanimQiz)'s Blog" />
+        <h1>Hello My friend</h1>
+        <Footer title="..." />
+      </div>
     </>
   );
 }
