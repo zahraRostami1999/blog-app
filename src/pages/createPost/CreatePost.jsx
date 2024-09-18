a609ee4c5bc8d5b304e182fc753100ced65629f4
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-18 22:21:24 +0330

    edit CreatePost page: add inputs

--- src/pages/createPost/CreatePost.jsx
+++ src/pages/createPost/CreatePost.jsx
@@ -1,10 +1,32 @@
-import styled from "createPost.module.css";
+import styled from "./createPost.module.css";
 import { Fragment } from "react";
+import Header from "../../Components/header/Header";
+import Input from "../../Components/input/Input";
+import { useState } from "react";
 
 function CreatePost() {
+  const [createPost, setCreatePost] = useState({
+    title: "",
+    author: "",
+    readingTime: "",
+  });
+
+  const handleChangePost = (e) => {
+    setCreatePost((prevState) => ({ 
+      ...prevState, 
+      [e.target.name]: e.target.value }));
+      console.log(createPost);
+      
+  };
+
   return (
     <>
-      <div></div>
+      <div>
+        <Header blogTitle="XanimQiz(Jeyran Qulu)'s Blog" />
+        <Input label="Title" name="title" handleChange={handleChangePost} />
+        <Input label="Author" name="author" handleChange={handleChangePost} />
+        <Input label="ReadingTime" name="readingTime" handleChange={handleChangePost} />
+      </div>
     </>
   );
 }
