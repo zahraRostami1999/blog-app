96e2c22a4dbccb0ec607c07fd66d05d17da35796
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-18 12:52:16 +0330

    edit Home page : remove Post_list import

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,5 @@
 import Header from "../../Components/header/Header";
 import Footer from "../../Components/footer/Footer";
-import Post_list from "../../Components/post_list/Post_list";
 import { Fragment } from "react";
 
 function Home() {
