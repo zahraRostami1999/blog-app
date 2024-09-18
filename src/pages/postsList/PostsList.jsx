dc87a2460aae0d091dc0c9d14046dc6545017776
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-18 12:48:39 +0330

    add Header, Footer and Post_list component to PostsList component

--- src/pages/postsList/PostsList.jsx
+++ src/pages/postsList/PostsList.jsx
@@ -1,11 +1,16 @@
 import { Fragment } from "react";
-
+import Header from "../../Components/header/Header";
+import Footer from "../../Components/footer/Footer";
+import Post_list from "../../Components/post_list/Post_list";
 
 function PostsList() {
-    return(
-        <>
-        </>
-    );
+  return (
+    <>
+      <Header blogTitle="Zahra Rostami(XanimQiz)'s Blog" />
+      <Post_list />
+      <Footer title="..." />
+    </>
+  );
 }
 
 export default PostsList;
