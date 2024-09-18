ce5aa5077665532c2c6e1941c31b4d0edb59999c
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-18 22:01:48 +0330

    edit App component: add create post rout

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/home/Home";
 import AboutUs from "./pages/aboutUs/AboutUs";
 import PostPage from "./pages/postPage/PostPage";
 import PostsList from "./pages/postsList/PostsList";
+import CreatePost from "./pages/createPost/CreatePost";
 import "./App.css";
 
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Posts" element={<PostsList />} />
+        <Route path="/Create-Post" element={<CreatePost />}/>  
         <Route path="/about" element={<AboutUs />} />
         <Route path="/Post/:id" element={<PostPage />}/>
       </Routes>
