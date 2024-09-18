b3e03ed00f04ee2737c8e3331953ebc60f147044
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-18 13:00:42 +0330

    add div with homeWrapper className to Home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,12 @@
 import Header from "../../Components/header/Header";
 import Footer from "../../Components/footer/Footer";
 import { Fragment } from "react";
-import styles from "./home.module.css";
+import styled from "./home.module.css";
 
 function Home() {
   return (
     <>
-      <div>
+      <div className={styled.homeWrapper}>
         <Header blogTitle="Zahra Rostami(XanimQiz)'s Blog" />
         <h1>Hello My friend</h1>
         <Footer title="..." />
