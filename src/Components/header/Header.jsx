f7c718e69e327841b3296c15a697597c0b29324e
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-18 22:02:46 +0330

    edit Header componnet: add create post button to navbar

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -1,12 +1,12 @@
 import styled from "./header.module.css";
 import logo from "./../../assets/image/profile.png";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Header(props) {
   return (
     <header className={styled.header}>
       <div className={styled.container}>
-        <div className={styled.logo}>
+        <div className={styled.logoContainer}>
           <div className={styled.logoImg}>
             <img src={logo} alt="logo" />
           </div>
@@ -14,14 +14,15 @@ function Header(props) {
             <h3>{props.blogTitle}</h3>
           </div>
         </div>
+
         <div className={styled.navbarContainer}>
           <div className={styled.navbar}>
             <Link to="/">Home</Link>
             <Link to="/Posts">Posts</Link>
+            <Link to="/Create-Post">Create Post</Link>
             <Link to="/about">About Us</Link>
           </div>
         </div>
-        <div className={styled.switch}> switch</div>
       </div>
     </header>
   );
