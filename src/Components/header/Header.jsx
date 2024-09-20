e575c0166edd23b3efd0c34991c1133047730b9c
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-20 20:15:51 +0330

    edit Header component: add new sections

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -1,33 +1,35 @@
 import styled from "./header.module.css";
 import logo from "./../../assets/image/profile.png";
 import { Link } from "react-router-dom";
+import { Fragment } from "react";
 
 function Header(props) {
   return (
-    <header className={styled.header}>
+    <>
       <div className={styled.container}>
-        <div className={styled.logoContainer}>
-          <div className={styled.logoImg}>
-            <img src={logo} alt="logo" />
-          </div>
-          <div className={styled.logoTitle}>
-            <h4>{props.blogName}</h4>
-            <div className={styled.blogTitleWrapper}>
-              <h1>{styled.blogTitle}</h1>
+        <div className={styled.headerContainer}>
+          <div className={styled.firstRow}>
+            <div className={styled.headerName}>
+              <h3>{props.blogName}</h3>
+            </div>
+            <div className={styled.navbarContainer}>
+              <div className={styled.navbar}>
+                <Link to="/">Home</Link>
+                <Link to="/Posts">Posts</Link>
+                <Link to="/Create-Post">Create Post</Link>
+                <Link to="/about">About Us</Link>
+              </div>
             </div>
           </div>
-        </div>
-
-        <div className={styled.navbarContainer}>
-          <div className={styled.navbar}>
-            <Link to="/">Home</Link>
-            <Link to="/Posts">Posts</Link>
-            <Link to="/Create-Post">Create Post</Link>
-            <Link to="/about">About Us</Link>
+          <div className={styled.secundRow}>
+            <div className={styled.blogTitleWrapper}>
+            <h1>{props.blogTitle}</h1>
+          </div>
           </div>
         </div>
       </div>
-    </header>
+    </>
+ 
   );
 }
 
