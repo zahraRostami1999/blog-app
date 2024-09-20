1471bfdfdd909e711f374e2a5d745d452e0f05c2
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-20 14:04:17 +0330

    edit Textarea component

--- src/Components/textArea/Textarea.jsx
+++ src/Components/textArea/Textarea.jsx
@@ -2,13 +2,14 @@ import styled from "./textarea.module.css";
 import { Fragment } from "react";
 
 function Textarea(props) {
-  return;
-  <>
-    <div>
-      <label>{props.label}</label>
-      <textarea name={props.name} onChange={props.handleChange}></textarea>
-    </div>
-  </>;
+  return (
+    <>
+      <div>
+        <label>{props.label}</label>
+        <textarea name={props.name} onChange={props.handleChange}></textarea>
+      </div>
+    </>
+  );
 }
 
 export default Textarea;
