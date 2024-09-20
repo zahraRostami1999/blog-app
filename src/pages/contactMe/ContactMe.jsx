7cd89d7594e0aef01945718cf85d62d55672b905
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-20 22:09:39 +0330

    edit Contact Me page

---
+++ src/pages/contactMe/ContactMe.jsx
@@ -0,0 +1,15 @@
+import Header from "../../Components/header/Header";
+import { Fragment } from "react";
+
+function ContactMe() {
+  return (
+    <>
+      <Header
+        blogName="Zahra Rostami (XanimQiz)' Blog"
+        blogTitle="Contact Me"
+      />
+    </>
+  );
+}
+
+export default ContactMe;
