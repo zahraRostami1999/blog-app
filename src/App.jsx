27d58e0c26143af66e2b82b76255ab6c6c33d4c1
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-20 22:13:20 +0330

    edit App component: edit contact me path

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
-import AboutUs from "./pages/aboutUs/AboutUs";
+import ContactMe from "./pages/contactMe/ContactMe";
 import PostPage from "./pages/postPage/PostPage";
 import PostsList from "./pages/postsList/PostsList";
 import CreatePost from "./pages/createPost/CreatePost";
@@ -14,7 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/Posts" element={<PostsList />} />
         <Route path="/Create-Post" element={<CreatePost />}/>  
-        <Route path="/about" element={<AboutUs />} />
+        <Route path="/contactMe" element={<ContactMe />} />
         <Route path="/Post/:id" element={<PostPage />}/>
       </Routes>
     </div>
