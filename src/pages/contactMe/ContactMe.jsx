ab12c3254af65947afa4c80d03c2ae5c5c4a5cd2
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-22 21:55:43 +0330

    edit contact Me: import phone and Envelope and Location icon

--- src/pages/contactMe/ContactMe.jsx
+++ src/pages/contactMe/ContactMe.jsx
@@ -3,9 +3,12 @@ import Header from "../../Components/header/Header";
 import { Fragment } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCoffee } from "@fortawesome/free-solid-svg-icons";
-import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
-import { faTelegram } from '@fortawesome/free-brands-svg-icons';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+import { faTelegram } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 
 function ContactMe() {
   return (
@@ -22,7 +25,20 @@ function ContactMe() {
                 <h1>Contact Information</h1>
                 <h3>Do you like say something?</h3>
               </div>
-              <div className={styled.contactInfo}></div>
+              <div className={styled.contactInfo}>
+                <div className={styled.phone}>
+                  <FontAwesomeIcon icon={faPhone} />
+                  <p>0123456789</p>
+                </div>
+                <div className={styled.mail}>
+                  <FontAwesomeIcon icon={faEnvelope} />
+                  <p>demo@gmail.com</p>
+                </div>
+                <div className={styled.location}>
+                  <FontAwesomeIcon icon={faLocationDot} />
+                  <p>Massachusetts 02156 United States</p>
+                </div>
+              </div>
               <div className={styled.socialIcon}>
                 <FontAwesomeIcon icon={faLinkedinIn} />
                 <FontAwesomeIcon icon={faTelegram} />
@@ -31,7 +47,31 @@ function ContactMe() {
             </div>
           </div>
           <div className={styled.col_60}>
-            <div className={styled.colContainer}></div>
+            <div className={styled.colContainer}>
+              <div className={styled.formContainer}>
+                <div className={styled.formGroup}>
+                  <label>First Name</label>
+                  <input type="text" placeholder="First Name" />
+                </div>
+
+                <div className={styled.formGroup}>
+                  <label>Last Name</label>
+                  <input type="text" value="Doe" />
+                </div>
+
+                <div className={styled.formGroup}>
+                  <label>Email</label>
+                  <input type="email" placeholder="Email" />
+                </div>
+
+                <div className={styled.formGroup}>
+                  <label>Phone Number</label>
+                  <div className={styled.phoneInput}>
+                    <input type="text" value="+1 012 3456 789" readOnly />
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
