9775f30d3cdf38abe866847a8421d61d3452105b
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-22 21:03:22 +0330

    edit contact Me: import social icons

--- src/pages/contactMe/ContactMe.jsx
+++ src/pages/contactMe/ContactMe.jsx
@@ -3,6 +3,9 @@ import Header from "../../Components/header/Header";
 import { Fragment } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { faTelegram } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 function ContactMe() {
   return (
@@ -15,13 +18,20 @@ function ContactMe() {
         <div className={styled.wrapper}>
           <div className={styled.col_40}>
             <div className={styled.colContainer}>
-
+              <div className={styled.titleInfo}>
+                <h1>Contact Information</h1>
+                <h3>Do you like say something?</h3>
+              </div>
+              <div className={styled.contactInfo}></div>
+              <div className={styled.socialIcon}>
+                <FontAwesomeIcon icon={faLinkedinIn} />
+                <FontAwesomeIcon icon={faTelegram} />
+                <FontAwesomeIcon icon={faInstagram} />
+              </div>
             </div>
           </div>
           <div className={styled.col_60}>
-            <div className={styled.colContainer}>
-
-            </div>
+            <div className={styled.colContainer}></div>
           </div>
         </div>
       </div>
