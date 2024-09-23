08768770460787573766ed1708cf86ce8a64df12
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-23 20:50:44 +0330

    edit Home page: edit home page content about me

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,15 +9,16 @@ function Home() {
       <Header blogName="Zahra Rostami(XanimQiz)" blogTitle="THE BLOG" />
       <div className={styled.coverContainer}>
         <div className={styled.backgroundCover}>
-          <h2>Hi there! I'm Zahra Rostami,</h2>
+          <h1>Welcome to My Blog!</h1>
+          <h2>
+            Hi, I’m Zahra Rostami, a passionate Frontend Developer specializing
+            in creating modern and responsive web applications. I focus on
+            training and working with the React programming language to build
+            dynamic, user-friendly interfaces.
+          </h2>
           <h3>
-            a front-end developer passionate about crafting engaging and
-            user-friendly web experiences. Currently, I'm diving deep into the
-            world of React, exploring its powerful capabilities and learning new
-            techniques to enhance my skills. Join me on this exciting journey as
-            I share insights, projects, and resources related to front-end
-            development, React, and anything else that sparks my interest. Feel
-            free to reach out if you have any questions or want to connect!
+            If you have any questions or would like to connect, feel free to
+            reach out through the Contact page. I’d love to hear from you!
           </h3>
         </div>
         <div className={styled.cover}>
