a233d9d7ba1a9e7806e878628df91f037e3759bd
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-24 15:32:44 +0330

    edit contactMe page: import background image

--- src/pages/contactMe/ContactMe.jsx
+++ src/pages/contactMe/ContactMe.jsx
@@ -11,53 +11,39 @@ import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import background from "../../assets/image/contactPage.jpg";
 
 function ContactMe() {
   return (
     <>
-      <Header
-        blogName="Zahra Rostami (XanimQiz)' Blog"
-        blogTitle="Contact Me"
-      />
-      <div className={styled.container}>
-        <div className={styled.wrapper}>
-          <div className={styled.col_40}>
-            <div className={styled.colContainer}>
-              <div className={styled.titleInfo}>
-                <h1>Contact Information</h1>
-                <h3>Do you like say something?</h3>
-              </div>
-              <div className={styled.contactInfo}>
-                <div className={styled.phone}>
-                  <FontAwesomeIcon icon={faPhone} />
-                  <p>0123456789</p>
+      <div style={{backgroundImage: `url(${background})`, backgroundSize: 'cover', height: '99.8vh'} }>
+        <Header blogName="Zahra Rostami (XanimQiz)' Blog" />
+        <div className={styled.container}>
+          <div className={styled.wrapper}>
+              <div className={styled.colContainer}>
+                <div className={styled.titleInfo}>
+                  <h2>Contact Information</h2>
+                  <h3>Do you like say something?</h3>
                 </div>
-                <div className={styled.mail}>
-                  <FontAwesomeIcon icon={faEnvelope} />
-                  <p>demo@gmail.com</p>
+                <div className={styled.contactInfo}>
+                  <div className={styled.phone}>
+                    <FontAwesomeIcon icon={faPhone} />
+                    <p>0123456789</p>
+                  </div>
+                  <div className={styled.mail}>
+                    <FontAwesomeIcon icon={faEnvelope} />
+                    <p>demo@gmail.com</p>
+                  </div>
+                  <div className={styled.location}>
+                    <FontAwesomeIcon icon={faLocationDot} />
+                    <p>Massachusetts 02156 United States</p>
+                  </div>
                 </div>
-                <div className={styled.location}>
-                  <FontAwesomeIcon icon={faLocationDot} />
-                  <p>Massachusetts 02156 United States</p>
+                <div className={styled.socialIcon}>
+                  <FontAwesomeIcon icon={faLinkedinIn} />
+                  <FontAwesomeIcon icon={faTelegram} />
+                  <FontAwesomeIcon icon={faInstagram} />
                 </div>
-              </div>
-              <div className={styled.socialIcon}>
-                <FontAwesomeIcon icon={faLinkedinIn} />
-                <FontAwesomeIcon icon={faTelegram} />
-                <FontAwesomeIcon icon={faInstagram} />
-              </div>
-            </div>
-          </div>
-          <div className={styled.col_60}>
-            <div className={styled.colContainer}>
-              <div className={styled.info}>
-                <Input label="First Name:" name="firstName" type="text" />
-                <Input label="Last Name:" name="lastName" type="text" />
-                <Input label="Email:" name="emailAddress" type="text"/>
-                <Input label="Phone Number:" name="phoneNumber" type="text"/>
-                <TextArea label="Message:" name="message" />
-                <button>Send Message</button>
-              </div>
             </div>
           </div>
         </div>
