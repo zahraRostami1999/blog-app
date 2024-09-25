145c8d8fdfd0b5aa9c1c3b3459abb0360bf52df4
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-25 19:47:48 +0330

    edit PostList page

--- src/pages/postsList/PostsList.jsx
+++ src/pages/postsList/PostsList.jsx
@@ -1,6 +1,5 @@
 import { Fragment } from "react";
 import Header from "../../Components/header/Header";
-import Footer from "../../Components/footer/Footer";
 import Post_list from "../../Components/post_list/Post_list";
 import background from '../../assets/image/postsList.jpg'
 
@@ -9,7 +8,7 @@ function PostsList() {
     <>
       <div style={{backgroundImage: `url(${background})`, backgroundSize: 'cover'}}>
         <Header
-          blogName="Zahra Rostami (XanimQiz)' Blog"
+          blogName="Zahra Rostami(XanimQiz)'s Blog"
         />
         <Post_list />
       </div>
