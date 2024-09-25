cc3d6a2e466600a726a644c2fe9b1d57477f393a
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-25 19:24:24 +0330

    edit post page: delete posts cover

--- src/Components/post/Post.jsx
+++ src/Components/post/Post.jsx
@@ -1,12 +1,11 @@
 import styled from "./post.module.css";
-import cover1 from "./../../assets/image/post1.png";
 
 function Post(props) {
   return (
     <div className={styled.postWrapper}>
       <div className={styled.postImg}>
-        {props.content.cover}
-        <img src={cover1} alt="cover" />
+        
+        <img src={props.content.cover} alt="cover" />
       </div>
       <div className={styled.postBadge}>{props.content.badge}</div>
       <div className={styled.postTitle}>{props.content.title}</div>
